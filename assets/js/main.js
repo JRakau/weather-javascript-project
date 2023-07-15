ec11f4a6b57f75e4084e0f9c5de20a0e8becef88
Josue Rakauskas <josuerakauskas@josues-air.home>
2023-07-15 20:44:50 +0100

    Add rule for Celcius and Fahrenheit, fixed setIcons()

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,57 +1,72 @@
+window.addEventListener("load", () => {
+  let long;
+  let lat;
+  let temperatureDescription = document.querySelector(
+    ".temperature-description"
+  );
+  let temperatureDegree = document.querySelector(".temperature-degree");
+  let locationTimezone = document.querySelector(".location-timezone");
+  let temperatureSection = document.querySelector(".temperature-section");
+  const temperatureSpan = document.querySelector(".temperature-section span");
 
-window.addEventListener('load', () => {
+  const keyapi = "8S2JKDDY6S8WGVTVPBJTKHJ67";
+  let apilink;
 
-    let long;
-    let lat;
-    let temperatureDescription = document.querySelector(".temperature-description");
-    let temperatureDegree = document.querySelector(".temperature-degree");
-    let locationTimezone = document.querySelector(".location-timezone");
-    console.log(temperatureDegree);
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
+      apilink = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${lat},${long}?key=${keyapi}`;
 
+      fetch(apilink)
+        .then((Response) => {
+          return Response.json();
+        })
+        .then((data) => {
+          console.log(data);
+          //const { temp } = data.currentConditions;
+          // set DOM Elements from the API
+          temperatureDegree.textContent = data.currentConditions.temp;
 
-    const keyapi = "8S2JKDDY6S8WGVTVPBJTKHJ67";
-    let apilink;
+          temperatureDescription.textContent = data.description;
 
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(position => {
-            long = position.coords.longitude;
-            lat = position.coords.latitude;
-            apilink = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${lat},${long}?key=${keyapi}`;
+          locationTimezone.textContent = data.timezone;
 
-            fetch(apilink)
-                .then(Response => {
-                    return Response.json();
-                })
-                .then(data => {
-                    console.log(data);
-                    //const { temp } = data.currentConditions;
-                    // set DOM Elements from the API
-                    temperatureDegree.textContent = data.currentConditions.temp;
-                  
-                    temperatureDescription.textContent = data.description;
+          //Set Icon
+          setIcons(
+            data.currentConditions.icon,
+            document.querySelector(".icon1")
+          );
 
-                    locationTimezone.textContent = data.timezone;
-                 
-                    //Set Icon
-                    setIcons(data.currentConditions.icon, document.querySelector(".icon1"));
-                });
+          //Change temperature to Celcius/Fahrenheit
+          temperatureSection.addEventListener("click", () => {
+            if (temperatureSpan.textContent === "F") {
+              temperatureSpan.textContent = "C";
+              temperatureDegree.textContent = Math.floor(
+                (data.currentConditions.temp - 32) * (5 / 9)
+              );
+            } else {
+              temperatureSpan.textContent = "F";
+              temperatureDegree.textContent = data.currentConditions.temp;
+            }
+          });
         });
+    });
+  } else {
+    /// not allowed geolocation use
+  }
 
-
-
-    } else {
-        /// not allowed geolocation use
-    }
-
-    function setIcons(icon, iconID) {
-        const skycons = new Skycons({ color: "white" });
-        const currentIcon = icon.replace(/-/g, "_").toUpperCase();
-        console.log(currentIcon);
-       
-        skycons.play();
-        return skycons.set(iconID, skycons[currentIcon]);
-    }
-
+  /**
+   * setIcons sets the icon to the skycons library
+   *
+   * @param {*} icon the icon to set
+   * @param {*} iconID the id of the icon to set
+   * @return {*} returns the icon
+   */
+  function setIcons(icon, iconID) {
+    const iconObj = new Skycons({ color: "white" });
+    const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+    iconObj.play();
+    return iconObj.set(iconID, Skycons[currentIcon]);
+  }
 });
-
-
